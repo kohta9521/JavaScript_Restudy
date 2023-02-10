5c5441bd09f5bec9349b0e2d6bebfd79985891f8
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-10 20:40:11 +0900

    add rank switch

---
+++ 2_5制御構文/2_43.js
@@ -0,0 +1,11 @@
+var rank = "B";
+
+if (rank === "A") {
+  console.log("Aランクです");
+} else if (rank == "B") {
+  console.log("Bランクです。");
+} else if (rank == "C") {
+  console.log("Cらんくです");
+} else {
+  console.log("ランク外です");
+}
