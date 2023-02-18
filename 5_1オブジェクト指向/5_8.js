e1d261b5d61c9061a1d1bcdbb300017726033ae4
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-18 18:43:38 +0900

    強制コンストラクター呼び出し

---
+++ 5_1オブジェクト指向/5_8.js
@@ -0,0 +1,9 @@
+var Member = function (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = laseName;
+};
+
+var m = Member("兵頭", "佐藤");
+console.log(m);
+console.log(firstName);
+console.log(lastName);
