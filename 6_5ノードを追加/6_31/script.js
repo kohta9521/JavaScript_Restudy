80672e14c8b53b9090a455fee14d00b4a31d0c45
Kohta9521 <kohta0421kochi@gmail.com>
2023-02-21 14:55:10 +0900

    add eventlistener error

---
+++ 6_5ノードを追加/6_31/script.js
@@ -0,0 +1,23 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var books = [
+      { title: "独習PHP", price: 3200 },
+      { title: "Javaポケットリファレンス", price: 2680 },
+      { title: "アプリを作ろう!!", price: 2000 },
+    ];
+
+    var flag = document.createDocumentFragment();
+
+    for (var i = 0, len = books.length; i < len; i++) {
+      var b = books[i];
+      var li = document.createElement("li");
+      var text = document.createTextNode(b.title + " : " + b.price + "円");
+      li.appenChild(text);
+      li.appendChild(li);
+    }
+
+    document.getElementById("list").appendChild(frag);
+  },
+  false
+);
